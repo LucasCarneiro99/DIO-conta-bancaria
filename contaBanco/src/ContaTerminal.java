2c0beb4973ba6930d9cb5c6dbf24d1bde08d1843
LucasCarneiro99 <lucasbcarneiro903@gmail.com>
2024-05-10 16:10:00 -0300

    conclusao da atividade DIO

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroConta;
+        String agencia, nomeCliente;
+        double saldoBancario;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome");
+        nomeCliente = scanner.nextLine();
+        System.out.println("Digite o número da conta");
+        numeroConta = scanner.nextInt();
+        System.out.println("Digite o número da agência");
+        agencia = scanner.next();
+        System.out.println("Digite o saldo da conta");
+        saldoBancario = scanner.nextDouble();
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta " + numeroConta+  " e seu saldo "+ saldoBancario + " já está disponível para saque");
+        scanner.close();
     }
 }
